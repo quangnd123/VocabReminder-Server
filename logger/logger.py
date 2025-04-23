8c8b470404de04a61bf41666a7075974830d51ad
quang <vinhquang001@e.ntu.edu.sg>
2025-04-23 18:24:02 +0700

    fix logger

--- logger/logger.py
+++ logger/logger.py
@@ -1,28 +1,21 @@
 import logging
-import os 
+import os
 
 base_dir = os.path.dirname(__file__)
 
 def get_logger(name: str, file_name: str) -> logging.Logger:
-    """
-    Gets a logger with the specified name.
-
-    Args:
-        name (str): The name of the logger.
-        file_name (str): Log to this file.
-
-    Returns:
-        logging.Logger: The logger.
-    """
-    # Configure logging
-    logging.basicConfig(
-        level=logging.INFO,  # Set logging level
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",  # Log format
-        datefmt="%Y-%m-%d %H:%M:%S",  # Timestamp format
-        encoding="utf-8",  # Encoding
-        filename=base_dir + "/logs/" + file_name,  # Log file
-        filemode="a"  # Append to log file
-    )
-
     logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    # Check if the logger already has the handler to prevent duplicate logs
+    if not any(isinstance(h, logging.FileHandler) and h.baseFilename.endswith(file_name) for h in logger.handlers):
+        log_path = os.path.join(base_dir, "logs", file_name)
+        os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
+        file_handler = logging.FileHandler(log_path, encoding="utf-8")
+        formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s", "%Y-%m-%d %H:%M:%S")
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+
     return logger
