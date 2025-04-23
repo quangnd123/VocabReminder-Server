77526f3cf6c7b29f773080db6d1167a46f513562
quang <vinhquang001@e.ntu.edu.sg>
2025-04-23 17:52:36 +0700

    fix a bug in lang detection

--- fasttext_092/language_detection.py
+++ fasttext_092/language_detection.py
@@ -5,6 +5,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 accurate_model = _FastText(os.path.join(script_dir, "lid.176.bin"))
 
 def detect_language(text: str):
+    text = text.replace("\n", " ")  # ensure single line
     predictions = accurate_model.predict(text=text, k=1)
     language_code = predictions[0][0].replace('__label__', '')
     confidence = predictions[1][0]
